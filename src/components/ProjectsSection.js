364135937ee642a285ca4dd0a5095c0452f4ba58
MoisesFC <37132345+MoisesFC@users.noreply.github.com>
2023-08-13 03:32:44 -0400

    updated project cards

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { Container, Card, Row, Col } from "react-bootstrap";
+import nucampsiteWebsiteImg from "../img/nucampWebsiteImage.jpg";
+import blogAppWebsiteImg from "../img/blogAppWebsiteImg.jpg";
+import summitBankWebsiteImg from "../img/summitBankWebsiteImg.jpg";
+import budgetPlanningWebsiteImg from "../img/budgetPlannerWebsiteImg.jpg";
+
 
 const ProjectsSection = () => {
     return (
-        <Container className="pt-5">
+        <Container className="pt-5 pb-5">
             <h1>Projects</h1>
             <hr />
             <Row>
-                <Col>
+                <Col xs={12} md={6} lg={4} className="pb-3">
                     <Card>
-                        <Card.Img></Card.Img>
+                        <a href="https://react-blog-a5f95.ue.r.appspot.com/" target="_blank" rel="noopener noreferrer">
+                            <Card.Img src={nucampsiteWebsiteImg} alt="nucampsite website thumbnail" />
+                        </a>
                         <Card.Body>
                             <Card.Title>Full Stack React Blog App</Card.Title>
                             <Card.Text>
@@ -18,9 +25,23 @@ const ProjectsSection = () => {
                         </Card.Body>
                     </Card>
                 </Col>
-                <Col>
+                <Col xs={12} md={6} lg={4}  className="pb-3">
                     <Card>
-                        <Card.Img></Card.Img>
+                        <a href="https://react-deploy-test-383704.web.app/" target="_blank" rel="noopener noreferrer">
+                            <Card.Img src={blogAppWebsiteImg} alt="nucampsite website thumbnail" />
+                        </a>
+                        <Card.Body>
+                            <Card.Title>Full Stack React Blog App</Card.Title>
+                            <Card.Text>
+
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+
+                <Col xs={12} md={6} lg={4}  className="pb-3">
+                    <Card>
+                        <a href="https://summit-bank-a5b6f.web.app/"><Card.Img src={summitBankWebsiteImg} alt="nucampsite website thumbnail" /></a>
                         <Card.Body>
                             <Card.Title>Summit Bank</Card.Title>
                             <Card.Text>
@@ -29,11 +50,11 @@ const ProjectsSection = () => {
                         </Card.Body>
                     </Card>
                 </Col>
-                <Col>
+                <Col xs={12} md={6} lg={4}  className="pb-3">
                     <Card>
-                        <Card.Img></Card.Img>
+                        <a href="https://budget-tracker-afeab.web.app/"><Card.Img src={budgetPlanningWebsiteImg} alt="nucampsite website thumbnail" /></a>
                         <Card.Body>
-                            <Card.Title>Nucamp Website</Card.Title>
+                            <Card.Title>Budget Planner</Card.Title>
                             <Card.Text>
 
                             </Card.Text>
