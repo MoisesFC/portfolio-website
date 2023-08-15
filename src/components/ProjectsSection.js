abd926b8237976fa68ae62c462cbda895969a978
MoisesFC <37132345+MoisesFC@users.noreply.github.com>
2023-08-15 03:20:19 -0400

    added modal to show screenshots of summit bank

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,22 +1,37 @@
 import React, { useState } from "react";
-import { Container, Card, Row, Col, Modal, Button } from "react-bootstrap";
+import { Container, Card, Row, Col, Modal, Button, Image } from "react-bootstrap";
 import nucampsiteWebsiteImg from "../img/nucampWebsiteImage.jpg";
 import blogAppWebsiteImg from "../img/blogAppWebsiteImg.jpg";
 import summitBankWebsiteImg from "../img/summitBankWebsiteImg.jpg";
 import budgetPlanningWebsiteImg from "../img/budgetPlannerWebsiteImg.jpg";
+import SummitBankCCImg from "../img/SummitBankCC.png";
+import SummitBankHomeImg from "../img/SummiytBankHome.png";
+import SummitBankCheckingImg from "../img/SummitBankChecking.png";
+import SummitBankRetirementImg from "../img/SummitBankRetirement.png";
 
 
 const ProjectsSection = () => {
 
-    const [showModal, setShowModal] = React.useState(false);
+    const [showWarningModal, setShowWarningModal] = React.useState(false);
 
     const openModal = () => {
-        setShowModal(true);
+        setShowWarningModal(true);
     };
 
     const closeModal = () => {
-        setShowModal(false);
+        setShowWarningModal(false);
     };
+
+    const [showBankImgsModal, setShowBankImgsModal] = React.useState(false);
+
+    const openBankImgsModal = () => {
+        setShowBankImgsModal(true);
+    };
+
+    const closeBankImgsModal = () => {
+        setShowBankImgsModal(false);
+    };
+
 
     return (
         <Container className="pt-5 pb-5">
@@ -29,13 +44,13 @@ const ProjectsSection = () => {
                         <a href="https://react-blog-a5f95.ue.r.appspot.com/" target="_blank" rel="noopener noreferrer">
                             <Card.Img src={blogAppWebsiteImg} alt="nucampsite website thumbnail" />
                         </a>
-                        <Card.Body style={{minHeight: "250px"}}>
+                        <Card.Body style={{ minHeight: "250px" }}>
                             <hr />
                             <Card.Title>Full-Stack React Blog App</Card.Title>
                             <Card.Text className="pt-2">
-                                Personal blogging app built with the complete suite of the MERN stack.<br/>
+                                Personal blogging app built with the complete suite of the MERN stack.<br />
                                 This site allows anyone to read the published articles, as well as to log in or create an account to interact with the posts by adding comments or adding upvotes.
-                                <br/>The project is powered by the Google Cloud Platform and Google Firebase.
+                                <br />The project is powered by the Google Cloud Platform and Google Firebase.
                             </Card.Text>
                             <a href="https://react-blog-a5f95.ue.r.appspot.com/" target="_blank" rel="noopener noreferrer"><Button >Go to Project</Button></a>
                         </Card.Body>
@@ -46,7 +61,7 @@ const ProjectsSection = () => {
                         <a href="https://react-deploy-test-383704.web.app/" target="_blank" rel="noopener noreferrer">
                             <Card.Img src={nucampsiteWebsiteImg} alt="nucampsite website thumbnail" />
                         </a>
-                        <Card.Body style={{minHeight: "250px"}}>
+                        <Card.Body style={{ minHeight: "250px" }}>
                             <hr />
                             <Card.Title>Nucamp Website</Card.Title>
                             <Card.Text>
@@ -60,7 +75,7 @@ const ProjectsSection = () => {
                 <Col xs={12} md={6} className="pb-3">
                     <Card>
                         <a href="#" onClick={openModal}><Card.Img src={summitBankWebsiteImg} alt="nucampsite website thumbnail" /></a>
-                        <Card.Body style={{minHeight: "250px"}}>
+                        <Card.Body style={{ minHeight: "250px" }}>
                             <hr />
                             <Card.Title>Summit Bank</Card.Title>
                             <Card.Text>
@@ -73,7 +88,7 @@ const ProjectsSection = () => {
                 <Col xs={12} md={6} className="pb-3">
                     <Card>
                         <a href="https://budget-tracker-afeab.web.app/" target="_blank" rel="noopener noreferrer"><Card.Img src={budgetPlanningWebsiteImg} alt="nucampsite website thumbnail" /></a>
-                        <Card.Body style={{minHeight: "250px"}}>
+                        <Card.Body style={{ minHeight: "250px" }}>
                             <hr />
                             <Card.Title>Budget Planner</Card.Title>
                             <Card.Text>
@@ -84,22 +99,44 @@ const ProjectsSection = () => {
                     </Card>
                 </Col>
             </Row>
-            <Modal show={showModal} onHide={closeModal}>
-                <Modal.Header closeButton>
+            <Modal show={showWarningModal} onHide={closeModal} >
+                <Modal.Header closeButton style={{backgroundColor: "#FFC866"}}>
                     <Modal.Title>Heads Up!</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     The following project is so awesome that it got flagged by Google.
                     I assume this has to do with the fact that this is a mock website for a fake bank, making them think that this is a phishing site.
                     If you get a red warning screen, click on the <strong>Details</strong> button and then click on the <span className="underline">visit this unsafe site</span> link. I am currently working with Google to have the flag removed.
-                    <br/>
-                    If you do not feel safe visiting the website, click <a href="google.com">here</a> to view some screenshots instead.
+                    <br />
+                    If you do not feel safe visiting the website, click <a href="#" onClick={openBankImgsModal}>here</a> to view some screenshots instead.
                 </Modal.Body>
                 <Modal.Footer>
-                <a href="https://summit-bank-a5b6f.web.app" target="_blank" rel="noopener noreferrer"><Button variant="primary" onClick={closeModal}>
-                        Go to the Website 
+                    <a href="https://summit-bank-a5b6f.web.app" target="_blank" rel="noopener noreferrer"><Button variant="primary" onClick={closeModal}>
+                        Go to the Website
                     </Button>
                     </a>
+                </Modal.Footer>
+            </Modal>
+            <Modal show={showBankImgsModal} onHide={closeBankImgsModal} size='xl'>
+                <Modal.Header closeButton>
+                    <Modal.Title>Summit Bank Project Screenshots</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p className="modalImgTxt">Home screen or landing page for the project:</p>
+                    <Image className="pb-5" src={SummitBankHomeImg} alt="SummitBank website screenshot Home" fluid />
+                    <p className="modalImgTxt">Checking account information page:</p>
+                    <Image className="pb-5" src={SummitBankCheckingImg} alt="SummitBank website screenshot Home" fluid />
+                    <p className="modalImgTxt">Credit card information page:</p>
+                    <Image className="pb-5" src={SummitBankCCImg} alt="SummitBank website screenshot Home" fluid />
+                    <p className="modalImgTxt">Retirement page with a fully functional retirement calculator:</p>
+                    <Image className="pb-5" src={SummitBankRetirementImg} alt="SummitBank website screenshot Home" fluid />
+                </Modal.Body>
+                <Modal.Footer>
+                    <a href="https://summit-bank-a5b6f.web.app" target="_blank" rel="noopener noreferrer"><Button variant="primary" onClick={closeModal}>
+                        Go to the Website
+                    </Button>
+                    </a>
+                    <Button variant="danger" onClick={closeBankImgsModal}>Close</Button>
                 </Modal.Footer>
             </Modal>
         </Container>
