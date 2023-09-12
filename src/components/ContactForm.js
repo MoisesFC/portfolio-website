3394275257ca5f586d22fb42aa9f68071d4321db
MoisesFC <37132345+MoisesFC@users.noreply.github.com>
2023-09-12 00:27:20 -0400

    connected form to my email and fixed the modal

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,62 +3,62 @@ import { Container, Modal, Button } from 'react-bootstrap'; // Import Modal and 
 import emailjs from '@emailjs/browser';
 
 const ContactForm = () => {
-  const form = useRef();
-  const [showModal, setShowModal] = useState(false); // State to control the modal visibility
+    const form = useRef();
+    const [showModal, setShowModal] = useState(false); // State to control the modal visibility
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+    const sendEmail = (e) => {
+        e.preventDefault();
 
-    emailjs
-      .sendForm('service_v5shoxz', 'template_07f6rnk', form.current, 'evvItdo30H67aKOLE')
-      .then((result) => {
-        console.log(result.text);
-        resetForm();
-        setShowModal(true); // Show the modal when the form is successfully submitted
-      })
-      .catch((error) => {
-        console.error(error.text);
-      });
-  };
+        emailjs
+            .sendForm('service_ep0fp0f', 'template_3d8prmi', form.current, 'a9NR3jPzuh_VRWGwZ')
+            .then((result) => {
+                console.log(result.text);
+                resetForm();
+                setShowModal(true); // Show the modal when the form is successfully submitted
+            })
+            .catch((error) => {
+                console.error(error.text);
+            });
+    };
 
-  const resetForm = () => {
-    const formElements = form.current.elements;
-    for (let i = 0; i < formElements.length; i++) {
-      if (formElements[i].type !== 'submit') {
-        formElements[i].value = '';
-      }
-    }
-  };
-  return (
-    <>
-      <Container className='mt-5'>
-        <h3 style={{ color: 'black', textAlign: 'center', textDecoration: "underline", marginBottom: "20px" }}>Contact Me</h3>
-        <form ref={form} onSubmit={sendEmail}>
-          <label style={{ color: 'black', textAlign: 'center' }}>Name</label>
-          <input required type="text" name="user_name" />
-          <label required style={{ color: 'black', textAlign: 'center' }}>Email</label>
-          <input type="email" name="user_email" />
-          <label required style={{ color: 'black', textAlign: 'center' }}>Phone</label>
-          <input type="phone" name="user_phone" />
-          <label style={{ color: 'black', textAlign: 'center' }}>Message</label>
-          <textarea name="message" />
-          <input className='mt-3' type="submit" value="Send" />
-        </form>
+    const resetForm = () => {
+        const formElements = form.current.elements;
+        for (let i = 0; i < formElements.length; i++) {
+            if (formElements[i].type !== 'submit') {
+                formElements[i].value = '';
+            }
+        }
+    };
+    return (
+        <>
+            <Container className='mt-5'>
+                <h3 style={{ color: 'black', textAlign: 'center', textDecoration: "underline", marginBottom: "20px" }}>Contact Me</h3>
+                <form ref={form} onSubmit={sendEmail}>
+                    <label style={{ color: 'black', textAlign: 'center' }}>Name</label>
+                    <input required type="text" name="from_name" />
+                    <label required style={{ color: 'black', textAlign: 'center' }}>Email</label>
+                    <input type="email" required name="from_email" />
+                    <label required style={{ color: 'black', textAlign: 'center' }}>Phone</label>
+                    <input type="phone" required name="from_phone" />
+                    <label  style={{ color: 'black', textAlign: 'center' }}>Message</label>
+                    <textarea required name="message" />
+                    <input className='mt-3' type="submit" value="Send" />
+                </form>
 
-        <Modal show={showModal} onHide={() => setShowModal(false)} backdrop="static">
-        <Modal.Header closeButton>
-          <Modal.Title>Form Submitted</Modal.Title>
-        </Modal.Header>
-        <Modal.Body><strong>Thank You!</strong> <br/>Your quote request has been successfully sumbmitted<br/> We will reach out to you soon.</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => setShowModal(false)}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-      </Container>
-    </>
-  );
+                <Modal show={showModal} onHide={() => setShowModal(false)} backdrop="static">
+                    <Modal.Header closeButton>
+                        <Modal.Title>Form Submitted</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body><strong>Thank You!</strong> <br />I have received your message and reach out to you soon.</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={() => setShowModal(false)}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </Container>
+        </>
+    );
 }
 
 export default ContactForm;
